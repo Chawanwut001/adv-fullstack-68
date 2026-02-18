a9892cd012db6f9d29c5467a3514aa10ab10c795
Chawanwut001 <s6506022620010@email.kmutnb.ac.th>
2026-02-18 10:57:53 +0700

    Lec9

--- AxiosNodeHtml.js
+++ AxiosNodeHtml.js
@@ -4,7 +4,7 @@ const app = express();
 var bodyParser = require('body-parser');
 const path = require('path');
 
-const base_url = 'http://localhost:5000';
+const base_url = 'http://localhost:3000';
 
 app.set("views", path.join(__dirname, "/public/views"));
 app.set('view engine', 'ejs');
